c4691fb28f08c6a4fcdd7f16f3b41ea985322a69
dewi22222222 <dewiksp972@gmail.com>
2024-08-31 13:07:32 +0700

    Penanganan Array dengan while Loop.php

---
+++ 19.Penanganan Array dengan while Loop.php
@@ -0,0 +1,10 @@
+<?php
+$fruits = ["apple", "banana", "cherry"];
+$i = 0;
+
+while ($i < count($fruits)) {
+    echo $fruits[$i] . "<br>";
+    $i++;
+}
+
+echo "<br><br><strong> by:dewi puspita sari";
