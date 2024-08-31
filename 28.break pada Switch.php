28c32196e8c9e1b039f590971b50f9e2c59d1a67
dewi22222222 <dewiksp972@gmail.com>
2024-08-31 13:43:42 +0700

    break pada switch.php

---
+++ 28.break pada Switch.php
@@ -0,0 +1,19 @@
+<?php
+$buah = "apel";
+
+switch ($buah) {
+    case "mangga":
+        echo "Ini mangga";
+        break;
+    case "apel":
+        echo "Ini apel";
+        // Break disini menghentikan switch
+        break;
+    case "pisang":
+        echo "Ini pisang";
+        break;
+    default:
+        echo "Buah tidak dikenal";
+}
+
+echo "<br><br><strong> by:dewi puspita sari";
