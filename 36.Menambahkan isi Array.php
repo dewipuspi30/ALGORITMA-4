678c6b2415448674d9c79a4e44fd3a832c1b07ea
dewi22222222 <dewiksp972@gmail.com>
2024-09-01 20:12:24 +0700

    M.enambahkan isi Array.php

---
+++ 36.Menambahkan isi Array.php
@@ -0,0 +1,21 @@
+<?php
+// membuat array
+$hobi = [
+    "Membaca",
+    "Menulis",
+    "Ngeblog"
+];
+
+// menambahkan isi pada idenks ke-3
+$hobi[3] = "Coding";
+
+// menambahkan isi pada indeks terakhir
+$hobi[] = "Olahraga";
+
+// cetak array dengan perulangan
+foreach($hobi as $hobiku){
+    echo $hobiku."<br>";
+}
+
+
+echo "<br><br><strong> by:dewi puspita sari";
