9afafaee12484d17ef554eabada95eb55ac42c06
dewi22222222 <dewiksp972@gmail.com>
2024-09-02 08:13:57 +0700

    Menyertakan nilai key.php

--- 39.Menyertakan nilai key.php
+++ 39.Menyertakan nilai key.php
@@ -1,10 +1,6 @@
 <?php
 $bulan = array ('Januari', 'Februari', 'Maret');
-foreach ($bulan as $nama_bulan) {
-	echo $nama_bulan . '<br/>';
-}
-$bulan = array ('Januari', 'Februari', 'Maret');
 foreach ($bulan as $index => $nama_bulan) {
 	echo ($index + 1) . '. ' . $nama_bulan . '<br/>';
-
+}
     echo "<br><br><strong> by:dewi puspita sari";
