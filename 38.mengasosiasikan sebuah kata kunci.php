f7f14ff297fb6f5f1350f1d1c3b9eb95dddbddf1
dewi22222222 <dewiksp972@gmail.com>
2024-09-17 09:11:48 +0700

    mengasosiasikan sebuah kata kunci.php

---
+++ 38.mengasosiasikan sebuah kata kunci.php
@@ -0,0 +1,10 @@
+<?php
+$email["subjek"] = "Apa Kabar?";
+$email["pengirim"] = "amel@amellya.com";
+$email["isi"] = "Apa kabar amel? sudah lama kita tidak berjumpa";
+
+echo "<pre>";
+print_r($email);
+echo "</pre>";
+
+echo "<p><strong>By dewi puspita sari</strong>";
--- 39.Menyertakan nilai key.php
+++
@@ -1,6 +0,0 @@
-<?php
-$bulan = array ('Januari', 'Februari', 'Maret');
-foreach ($bulan as $index => $nama_bulan) {
-	echo ($index + 1) . '. ' . $nama_bulan . '<br/>';
-}
-    echo "<br><br><strong> by:dewi puspita sari";
